11034a0bc59b1e1dfa9d19a2758cfff2069b11da
Chris Wojcik <cpw1485@gmail.com>
2013-01-21 10:50:14 -0500

    added google analytics

--- live/index.php
+++ live/index.php
@@ -346,6 +346,13 @@ if (isset($_SESSION['return_data'])) {
     <script src="js/plugins.js"></script>
     <script src="js/script.js"></script>
     <!--<![endif]-->
+    
+    <script>
+        var _gaq=[['_setAccount','UA-27635613-2'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
 </body>
 
 </html>
